4d6d1b4716fc9126f74215f126986832c499528d
Sofia Ruiz <sofi.ruizsuarez@gmail.com>
2019-05-28 12:33:25 -0300

    add calculation of the residuals in R

--- fit_ArBayesian.R
+++ fit_ArBayesian.R
@@ -15,6 +15,7 @@ for (i in 1:length(eating_tss))
   ts.s=eating_tss[[i]]
   n.s[i]=length(ts.s$Acx)
 }
+ts.lengths=n.s
 n.s=c(1,n.s)
 
 combined.Acx=c()
@@ -53,7 +54,7 @@ resdims[1]=0
 
 ts_dat <- list(N = N, y = combined.Acx,M=M, ydims=cumsum(n.s),rdims=resdims)
 stanc("model_ar1.stan")
-fit1 <- stan(file = 'model_ar1.stan', data = ts_dat,chain=4,cores=3)
+fit1 <- stan(file = 'model_ar1.stan', data = ts_dat,chain=2,cores=3)
 
 #######
 # Out #
@@ -89,6 +90,8 @@ qqline(residuals)
 ######## AR(k) ########
 #######################
 
+## 1.Run stan model
+
 N=length(combined.Acx)
 K=3
 n.s[1]=0
@@ -97,6 +100,8 @@ M=length(n.s)-1
 q1=cumsum(n.s)[1:(length(n.s)-1)]+1
 q2=c(cumsum(n.s)[2:length(n.s)])
 ns=n.s[2:length(n.s)]
+y = combined.Acx
+
 ts_data <- list(N = N, y = combined.Acx,K=K,M=M,q1=q1,q2=q2,ns=ns,Npred=100)
 
 stanc("model_ark.stan")
@@ -107,42 +112,72 @@ fitk <- stan(file = 'model_ark.stan', data = ts_data,chain=3,cores=3)
 # Out #
 #######
 
-## Chains
+## 2. Chains
 traceplot(fitk,pars=c("alpha", "beta", "sigma"))
 outs.k <- rstan::extract(fitk, permuted = TRUE) # return a list of arrays 
 
-## Parameter estimation
+## 3. Parameter estimation
 print(fitk, pars=c("alpha", "beta", "sigma", "lp__"), probs=c(.1,.5,.9))
 plot(fitk,pars=c("alpha", "beta", "sigma"))
 
-## Posteriors
-hist(la$alpha)
-hist(la$beta)
-hist(la$sigma)
+## 3.1. Posteriors
+hist(outs.k$alpha)
+hist(outs.k$beta[,1])
+hist(outs.k$beta[,2])
+hist(outs.k$beta[,3])
+hist(outs.k$sigma)
 
-# Estacionalidad ? ---> mirar si los betas quedan dentro de la region Cp (si cumplen polinomio)
+## 4. Seasonality? ---> mirar si los betas quedan dentro de la region Cp (si cumplen polinomio)
 
-# Residuals. Calculation in R (BIEN ESTO ?)
+## 5. Residuals. 
+## 5.1 Calculation in R (BIEN ESTO ?)
 
-residuals=numeric(length(ts_data$y))
+n.posterior=length(outs.k$alpha)
+which=sample(seq(1,n.posterior),size=n.posterior)
 
-beta.sam=sample(1,la$beta)  
-alpha.sam=sample(1,la$alpha)  
+beta.sam=outs.k$beta[which,]  
+alpha.sam=outs.k$alpha[which]  
 
+residuals=matrix(NA,ncol=n.posterior,nrow=length(ts_data$y)-M*K)
 
-ind1=1;
-ind2=q2[1]-K;
-for (i in 1:M)
+sum(is.na(residuals))
+for (j in 1:n.posterior)
 {
-residuals[ind1:ind2]=y[(q1[m]+K):(q2[m])]-(alpha.sam+beta.sam * y[q1[m]:q2[m]])
-}
+  ind=1
+  for(m in 1:M)
+  {
+  for (n in K:(ts.lengths[m]-1))
+  {
+    residuals[ind,j]=y[q1[m]+n]-(alpha.sam[j]+t(beta.sam[j,])%*%y[(q1[m]+n-K):(q1[m]+n-1)])
+    ind=ind+1
+    }
+    
+  }  
+} 
 
 
 
+## 5.2 check independency and normality
+
+nsim=4
+hist(residuals[,nsim])
+qqnorm(residuals[,nsim])
+qqline(residuals[,nsim])
+shapiro.test(residuals[,nsim]) # bajo, rechazamos H0
+
+
+acf(residuals[,nsim])
+pacf(residuals[,nsim])
 
 
 
-### ypreds: one for each sample of the posterior distribution 
+## 6. Posterior predictive checks: to see if the model fit well to the data and compare models
+
+#ypreds: one for each sample of the posterior distribution 
 # Example: 
 plot(outs.k$ypred[7,],type='l')
+
+
+## 7. BIC(Prado) and DIC
+
 
