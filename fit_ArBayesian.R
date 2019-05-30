f198cd193238fb7d6e9390a497f50849404f4cb5
Sofia Ruiz <sofi.ruizsuarez@gmail.com>
2019-05-30 10:02:01 -0300

    loops over k and change residuals calculation (using mean posterior)

--- fit_ArBayesian.R
+++ fit_ArBayesian.R
@@ -93,7 +93,7 @@ qqline(residuals)
 ## 1.Run stan model
 
 N=length(combined.Acx)
-K=3
+K=seq(from=1,to=3,by=1)
 n.s[1]=0
 M=length(n.s)-1
 
@@ -101,59 +101,86 @@ q1=cumsum(n.s)[1:(length(n.s)-1)]+1
 q2=c(cumsum(n.s)[2:length(n.s)])
 ns=n.s[2:length(n.s)]
 y = combined.Acx
+fit.k=list()
 
-ts_data <- list(N = N, y = combined.Acx,K=K,M=M,q1=q1,q2=q2,ns=ns,Npred=100)
-
+# fit AR(k) for k in K
+for (j in 1:length(K))
+{
+ts_data <- list(N = N, y = combined.Acx,K=K[j],M=M,q1=q1,q2=q2,ns=ns,Npred=100)
 stanc("model_ark.stan")
-fitk <- stan(file = 'model_ark.stan', data = ts_data,chain=3,cores=3)
-
+fit.k[[j]] <- stan(file = 'model_ark.stan', data = ts_data,chain=3,cores=3)
+}
 
 #######
 # Out #
 #######
 
+outs.k=list()
+
+
 ## 2. Chains
-traceplot(fitk,pars=c("alpha", "beta", "sigma"))
-outs.k <- rstan::extract(fitk, permuted = TRUE) # return a list of arrays 
+for (p in 1:length(K))
+{
+traceplot(fit.k[[p]],pars=c("alpha", "beta", "sigma"))
+outs.k[[p]] <- rstan::extract(fit.k[[p]], permuted = TRUE) # return a list of arrays 
+}
 
 ## 3. Parameter estimation
-print(fitk, pars=c("alpha", "beta", "sigma", "lp__"), probs=c(.1,.5,.9))
-plot(fitk,pars=c("alpha", "beta", "sigma"))
+print(fit.k[[p]], pars=c("alpha", "beta", "sigma", "lp__"), probs=c(.1,.5,.9))
+plot(fit.k[[p]],pars=c("alpha", "beta", "sigma"))
+
 
 ## 3.1. Posteriors
-hist(outs.k$alpha)
-hist(outs.k$beta[,1])
-hist(outs.k$beta[,2])
+hist(outs.k[[p]]$alpha)
+hist(outs.k[[p]]$beta[,1])
+hist(outs.k[[p]]$beta[,2])
 hist(outs.k$beta[,3])
-hist(outs.k$sigma)
+hist(outs.k[[p]]$sigma)
 
 ## 4. Seasonality? ---> mirar si los betas quedan dentro de la region Cp (si cumplen polinomio)
 
 ## 5. Residuals. 
-## 5.1 Calculation in R (BIEN ESTO ?)
+## 5.1 Calculation in R 
 
-n.posterior=length(outs.k$alpha)
-which=sample(seq(1,n.posterior),size=n.posterior)
-
-beta.sam=outs.k$beta[which,]  
-alpha.sam=outs.k$alpha[which]  
-
-residuals=matrix(NA,ncol=n.posterior,nrow=length(ts_data$y)-M*K)
-
-sum(is.na(residuals))
-for (j in 1:n.posterior)
+residuals=list()
+  
+# usamos la media de las posteriores
+for (j in 1:length(K))
 {
+  residuals[[j]]=numeric(length(ts_data$y)-M*K[j])
+  beta.p=apply(outs.k[[j]]$beta,MARGIN=2,FUN=mean)
+  alpha.p=mean(outs.k[[j]]$alpha)
   ind=1
-  for(m in 1:M)
-  {
-  for (n in K:(ts.lengths[m]-1))
-  {
-    residuals[ind,j]=y[q1[m]+n]-(alpha.sam[j]+t(beta.sam[j,])%*%y[(q1[m]+n-K):(q1[m]+n-1)])
-    ind=ind+1
-    }
-    
-  }  
-} 
+  for (m in 1:M)
+  {    
+  for (n in (K[j]):(ts.lengths[m]-1))
+    {
+         residuals[[j]][ind]=y[q1[m]+n]-(alpha.p+t(beta.p)%*%y[(q1[m]+n-K[j]):(q1[m]+n-1)])
+        ind=ind+1
+         }
+  
+  }
+}
+
+
+#n.posterior=length(outs.k$alpha)
+#which=sample(seq(1,n.posterior),size=n.posterior)
+#residuals=matrix(NA,ncol=n.posterior,nrow=length(ts_data$y)-M*K)
+
+
+# for (j in 1:n.posterior)
+# {
+#   ind=1
+#   for(m in 1:M)
+#   {
+#   for (n in K:(ts.lengths[m]-1))
+#   {
+#     residuals[ind,j]=y[q1[m]+n]-(alpha.sam[j]+t(beta.sam[j,])%*%y[(q1[m]+n-K):(q1[m]+n-1)])
+#     ind=ind+1
+#     }
+#     
+#   }  
+# } 
 
 
 
@@ -169,6 +196,7 @@ shapiro.test(residuals[,nsim]) # bajo, rechazamos H0
 acf(residuals[,nsim])
 pacf(residuals[,nsim])
 
+ts.plot(as.ts(residuals))
 
 
 ## 6. Posterior predictive checks: to see if the model fit well to the data and compare models
@@ -178,6 +206,8 @@ pacf(residuals[,nsim])
 plot(outs.k$ypred[7,],type='l')
 
 
-## 7. BIC(Prado) and DIC
+## 7. BIC(Prado) and WAIC
+# log_likelihood=extract_log_lik(fit.k[[1]],parameter_name = c('beta','alpha'),merge_chains = T)
+# dim(log_likelihood)
 
 
