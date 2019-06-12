476e7c88711c0e577a97da8c87d77da230b184ec
Sofia Ruiz <sofi.ruizsuarez@gmail.com>
2019-06-12 10:13:48 -0300

    agregue vigilance y searching behaviour

--- group_ts.R
+++ group_ts.R
@@ -52,3 +52,28 @@ for (i in 1:length(Vigilance))
 }
 
 }
+
+############################################################################
+############################ Beahaviour: Searching #########################
+############################################################################
+if (beha=='searching')
+{
+  searching_tss=list()
+  for (i in 1:length(Searching))
+  {
+    ts=Searching[[i]] %>% select(Acc_x,Acc_y,Acc_z,VeDBA,Pitch.angle,DateTime)
+    
+    # Windows summarize every (1/2 min)
+    
+    w=floor(40/2)
+    n=length(ts$Acc_x)
+    group=rep(seq(1,floor(n/w)),each=w)
+    group=c(group,rep(floor(n/w)+1,n-length(group)))
+    
+    ts$group=as.factor(group)
+    searching_tss[[i]]=ts %>% group_by(group) %>% summarise(Acx=mean(Acc_x),Acy=mean(Acc_y),
+                                                            Acz=mean(Acc_z),Pitch=mean(Pitch.angle),VeDBA=mean(VeDBA))
+    
+  }
+  
+}
